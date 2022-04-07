37e83ef021371c42ff6b45176b50b83d11e20b23
Matuz Bence <matuzb97@gmail.com>
2022-04-07 21:52:54 +0200

    txt read
    i can read data from file

--- plot.py
+++ plot.py
@@ -15,3 +15,20 @@ self = Airplane()
 self.attitude = 1000
 self.heading = 180
 self.id = 1
+
+airplane2 = Airplane()
+airplane2.attitude =  lines[0]
+airplane2.heading = lines[1]
+aLeinght = len(lines)
+i=0
+while aLeinght != i:
+
+    airplane2.attitude.append(lines[i+2])
+    airplane2.heading.append(lines[i+3])
+    ++i
+
+
+
+
+
+
